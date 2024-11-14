f381fd4f4fbae6b2d22fe42da224a6ec97cc8150
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2024-11-14 21:32:04 +0900

    11/14

--- src/sort/SortOperator.java
+++ src/sort/SortOperator.java
@@ -15,37 +15,59 @@ public class SortOperator {
             for(int i=0; i<arr.length; i++){
                 arr[i] = ran.nextInt(-100, 100);
             }
-            so.show(arr);
             System.out.print("1.선택 2.삽입 3.병합 4.힙 5.퀵\n입력 : ");
-            switch (sc.nextLine()) {
+            String in = sc.nextLine();
+            System.out.print("초기 ");
+            so.show(arr);
+            switch (in) {
                 case "1": {
-                    continue;
+                    so.selectSort(arr);
+                    break;
                 }
                 case "2": {
-                    continue;
+                    break;
                 }
                 case "3": {
-                    continue;
+                    break;
                 }
                 case "4": {
-                    continue;
+                    break;
                 }
                 case "5": {
-                    continue;
+                    break;
                 }
                 default:
                     break loop;
             }
+            System.out.println();
         }
     }
 
-    /*int[] selectSort(int[] arr){
-
+    void selectSort(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            int max = 0;
+            for(int j=0; j< arr.length-i; j++){
+                if(arr[max] < arr[j]){
+                    max = j;
+                }
+            }
+            if(i != max) {
+                int temp = arr[max];
+                arr[max] = arr[arr.length - i - 1];
+                arr[arr.length - i - 1] = temp;
+            }
+            System.out.print(i+1+"회 정렬 후 ");
+            show(arr);
+        }
     }
-    int[] insertSort(int[] arr){
 
+    void insertSort(int[] arr){
+        for(int i=0; i<arr.length; i++){
+
+        }
     }
-    int[] mergeSort(int[] arr){
+
+    /*int[] mergeSort(int[] arr){
 
     }
     int[] heapSort(int[] arr){
@@ -60,6 +82,6 @@ public class SortOperator {
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
-        System.out.println("\n");
+        System.out.println();
     }
 }
